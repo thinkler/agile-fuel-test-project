130d8e05851d025528f7baca95e122b985296770
Oleg Mak <anslapidus@gmail.com>
2018-04-16 12:59:40 +0300

    Refactor rails views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,3 @@
 module ApplicationHelper
-  def sortable(column)
-    title ||= column.titleize
-    old_order = params[:order] || ''
-    direction = '-'
-
-    direction = '' if old_order[0] == '-' || !old_order.include?(column)
-
-    link_to title, request.params.merge(order: direction + column)
-  end
-
-  def apply_status_filter(status)
-    link_to format_status(status), request.params.merge(filters: { status: status }, page: 1)
-  end
-
-  def format_status(status)
-    status.tr('_', ' ').capitalize
-  end
-
+  include RewardHelpers
 end
---
+++ app/helpers/reward_helpers.rb
@@ -0,0 +1,18 @@
+module RewardHelpers
+  def sortable(column)
+    title ||= column.titleize
+
+    direction = params[:order] && params[:order] == column ? '-' : ''
+    order = direction + column
+
+    link_to title, request.params.merge(order: order)
+  end
+
+  def apply_status_filter(status)
+    link_to format_status(status), request.params.merge(status: status, page: 1)
+  end
+
+  def format_status(status)
+    status.tr('_', ' ').capitalize
+  end
+end
